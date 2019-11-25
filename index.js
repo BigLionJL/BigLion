aa2cf172620f4db459131d8dd4c846d354641532
BigLionJL <58151505+BigLionJL@users.noreply.github.com>
2019-11-25 14:30:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,18 +20,7 @@ client.on('message', message => {
     }
 }
 	if (message.content === '!jail') {
-        if(message.member.roles.some(role => role.name === 'Mods')) {
-            let role = message.guild.roles.find(r => r.name === "Gay Baby Jail");
-        let member = message.mentions.members.first();
-
-        if(member.roles.some(r=>["Gay Baby Jail"].includes(r.name)) ) {
-            member.removeRole(role).catch(console.error);
-          } else {
-            member.addRole(role).catch(console.error);
-          }
-          } else {
-            message.channel.send(`Nope, noppers, nadda.`);
-          }
+        message.channel.send('Test');
         
     }
 }
