289ffaf659263d6d09c3f672fa4f57596a586342
BigLionJL <58151505+BigLionJL@users.noreply.github.com>
2019-11-25 10:01:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ client.on('message', message => {
     }
 }
     if (message.content === '!jail') {
+	    message.channel.send('This is a test');
         if(message.member.roles.some(role => role.name === 'Mods')) {
             let role = message.guild.roles.find(r => r.name === "Gay Baby Jail");
-		message.channel.send('This is a test');
         let member = message.mentions.members.first();
 
         if(member.roles.some(r=>["Gay Baby Jail"].includes(r.name)) ) {
