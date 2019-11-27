96ec66187376f663fcba27401eb5cf1f412c8ea6
BigLionJL <58151505+BigLionJL@users.noreply.github.com>
2019-11-27 12:29:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('message', message => {
 	  if(message.member.roles.find(r => r.name === "Admin") || message.member.roles.find(r => r.name === "Mods")){
 		if(message.member.roles.find(r => r.name === "Admin") || message.member.roles.find(r => r.name === "Mods")){
 		  let role = message.guild.roles.find(r => r.name === "Jail");
-		  let member = message.mentions.members.first();
+		  let member = message.member;
 		 member.addRole(role).catch(console.error);
 		  } else {
 			message.channel.send('Test');
