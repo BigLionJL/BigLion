9e7586da250933054b7096a5432dd632b3e10f83
BigLionJL <58151505+BigLionJL@users.noreply.github.com>
2019-11-27 12:19:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ client.on('message', message => {
 	if (message.content === '!jail') {
 	  if(message.member.roles.find(r => r.name === "Admin") || message.member.roles.find(r => r.name === "Mods")){
 		  let role = message.guild.roles.find(r => r.name === "Jail");
+		  let member = = message.member;
 		 member.addRole(role).catch(console.error);
 		  } else {
 			message.channel.send('Test');
